3e368e7c3b16e2557326b3a74ec326251acefea6
yuanweimin <18758554564@163.com>
2018-11-21 14:24:14 +0800

    first commit

---
+++ src/main/java/com/ywm/demo/ShopManageApplication.java
@@ -0,0 +1,12 @@
+package com.ywm.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopManageApplication.class, args);
+	}
+}
